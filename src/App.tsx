8ae3bbfbcd7215b24da1fc78ac3b3243f2dbe21a
Samuele Battistella <battistar92@gmail.com>
2022-12-28 16:14:53 +0100

    Fix gh pages refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,34 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Error from './pages/Error';
 import DrinkList, { loader as drinkListLoader } from './pages/DrinkList';
 import Drink, { loader as drinkLoader } from './pages/Drink';
 import Root, { loader as rootLoader } from './pages/Root';
 
-const basename = (): string => {
-  if (process.env.NODE_ENV === 'production') {
-    return '/poison';
-  }
-
-  return '/';
-};
-
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Root />,
-      loader: rootLoader,
-      errorElement: <Error />,
-      children: [
-        {
-          index: true,
-          element: <DrinkList />,
-          loader: drinkListLoader,
-        },
-        {
-          path: 'category/:category',
-          element: <DrinkList />,
-          loader: drinkListLoader,
-        },
-        {
-          path: 'category/:category/:id',
-          element: <Drink />,
-          loader: drinkLoader,
-        },
-      ],
-    },
-  ],
+const router = createHashRouter([
   {
-    basename: basename(),
-  }
-);
+    path: '/',
+    element: <Root />,
+    loader: rootLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <DrinkList />,
+        loader: drinkListLoader,
+      },
+      {
+        path: 'category/:category',
+        element: <DrinkList />,
+        loader: drinkListLoader,
+      },
+      {
+        path: 'category/:category/:id',
+        element: <Drink />,
+        loader: drinkLoader,
+      },
+    ],
+  },
+]);
 
 const App = (): JSX.Element => {
   return <RouterProvider router={router} />;
